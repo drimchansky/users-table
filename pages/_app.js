f43c8e8379207e30abd8ab0aca5bf2b4d5465daa
Nikita Chernov <drimchansky@gmail.com>
2020-12-09 17:42:29 +0300

    Container, global-styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
-import "../utils/fonts.css";
-import "normalize.css";
+import '../utils/fonts.css'
+import 'normalize.css'
+import '../utils/global-styles.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
+// components
 import Head from 'next/head'
+import Container from '../components/Container'
 
 export default function Home() {
   return (
@@ -10,7 +12,7 @@ export default function Home() {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
-      <h1>Hello</h1>
+      <Container>Имя пользователя</Container>
     </>
   )
 }
