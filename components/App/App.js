0f76e2185c7bd95fd9aa39100701848db4c808cd
Nikita Chernov <drimchansky@gmail.com>
2020-12-09 21:27:39 +0300

    Base display users

--- components/App/App.js
+++ components/App/App.js
@@ -6,18 +6,23 @@ import Container from '../Container'
 import Header from '../Header'
 import SearchBar from '../SearchBar'
 import Main from '../Main'
+// styles
+import styles from './App.module.css'
 
 const App = () => {
   const [appState, setAppState] = useState({ loading: true, error: false })
   const [users, setUsers] = useState(null)
-  const [blackList, setBlackList] = useState()
+  const [blackList, setBlackList] = useState([])
+
+  console.log(blackList)
 
   useEffect(() => {
     axios.get('https://5ebbb8e5f2cfeb001697d05c.mockapi.io/users').then(
       (response) => {
         setUsers(response)
-        console.log('fetched')
-        setAppState(false)
+        setTimeout(() => {
+          setAppState({ loading: false })
+        }, 1000)
       },
       (error) => {
         appState({ error: true })
@@ -29,7 +34,11 @@ const App = () => {
     <Container>
       <Header>Список пользователей</Header>
       <SearchBar />
-      <Main appState={appState} />
+      {appState.loading ? (
+        <div className={styles.caption}>Загрузка...</div>
+      ) : (
+        <Main users={users} />
+      )}
     </Container>
   )
 }
--- components/Main/Main.js
+++ components/Main/Main.js
@@ -3,7 +3,7 @@ import React from 'react'
 // styles
 import styles from './Main.module.css'
 
-const Main = ({ appState }) => {
+const Main = ({ users }) => {
   return (
     <main className={styles.main}>
       <div className={styles.row}>
@@ -11,21 +11,38 @@ const Main = ({ appState }) => {
       </div>
       <div className={styles.wrapper}>
         <table className={styles.table} border="1px">
-          <tr className={styles.row}>
-            <th className={styles.col}>Имя пользователя</th>
-            <th className={styles.col}>E-mail</th>
-            <th className={styles.col}>Дата регистрации</th>
-            <th className={styles.col}>Рейтинг</th>
-          </tr>
-          <tr className={styles.row}>
-            <td className={styles.col}>34,5</td>
-            <td className={styles.col}>3,5</td>
-            <td className={styles.col}>36</td>
-            <td className={styles.col}>23</td>
-          </tr>
+          <tbody>
+            <tr className={styles.row}>
+              <th className={styles.col}>
+                <button>Имя пользователя</button>
+              </th>
+              <th className={styles.col}>
+                <button>E-mail</button>
+              </th>
+              <th className={styles.col}>
+                <button>Дата регистрации</button>
+              </th>
+              <th className={styles.col}>
+                <button>Рейтинг</button>
+              </th>
+              <th className={styles.col}></th>
+            </tr>
+
+            {users.data.map((user) => {
+              return (
+                <tr className={styles.row} key={user.id}>
+                  <td className={styles.col}>{user.username}</td>
+                  <td className={styles.col}>{user.email}</td>
+                  <td className={styles.col}>{user.registration_date}</td>
+                  <td className={styles.col}>{user.rating}</td>
+                  <td className={styles.col}>
+                    <button>x</button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
         </table>
-        {appState ? <span className={styles.loading}>Загрузка...</span> : ''}
-        <div className={styles.loading}>Загрузка...</div>
       </div>
     </main>
   )
